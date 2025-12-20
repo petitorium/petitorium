617a3807f536bab887e72028ab27906e3676c1ba
Héctor Barral <hbarral@mail.ru>
2025-12-20 20:07:38 -0300

    refactor: extract cycle types to new file and reorganize data management logic
    - Create `cmd/cycle_types.go` with Cycle interface and implementations (MainCycle, URLBarCycle, HeadersCycle, EnvironmentsCycle, WorkspaceCycle)
    - Move cycle-related code from `cmd/cycles.go` to `cycle_types.go` and update imports
    - Move request finding functions from `cycles.go` to `DataManager` methods in `cmd/data_management.go` for better encapsulation

--- cmd/data_management.go
+++ cmd/data_management.go
@@ -21,7 +21,44 @@ func NewDataManager(workspaceData *workspace.Workspace) *DataManager {
 
 // FindRequestPtr finds the pointer to a request in the workspace data
 func (dm *DataManager) FindRequestPtr(targetReq workspace.Request) *workspace.Request {
-	return findRequestPtr(dm.workspaceData, targetReq)
+	return dm.findRequestPtr(dm.workspaceData, targetReq)
+}
+
+// findRequestPtr finds the request pointer in workspace collections
+func (dm *DataManager) findRequestPtr(data *workspace.Workspace, req workspace.Request) *workspace.Request {
+	// Search in collections
+	for i := range data.Collections {
+		for j := range data.Collections[i].Requests {
+			if data.Collections[i].Requests[j].Name == req.Name &&
+				data.Collections[i].Requests[j].Method == req.Method &&
+				data.Collections[i].Requests[j].URL == req.URL &&
+				data.Collections[i].Requests[j].Body == req.Body {
+				return &data.Collections[i].Requests[j]
+			}
+		}
+		if ptr := dm.findRequestPtrInCollections(data.Collections[i].Collections, req); ptr != nil {
+			return ptr
+		}
+	}
+	return nil
+}
+
+// Helper function to find in nested collections
+func (dm *DataManager) findRequestPtrInCollections(data []workspace.Collection, req workspace.Request) *workspace.Request {
+	for i := range data {
+		for j := range data[i].Requests {
+			if data[i].Requests[j].Name == req.Name &&
+				data[i].Requests[j].Method == req.Method &&
+				data[i].Requests[j].URL == req.URL &&
+				data[i].Requests[j].Body == req.Body {
+				return &data[i].Requests[j]
+			}
+		}
+		if ptr := dm.findRequestPtrInCollections(data[i].Collections, req); ptr != nil {
+			return ptr
+		}
+	}
+	return nil
 }
 
 // SaveWorkspace saves the workspace data to disk
